3f672a80dd27cff80e5c8a8388ec37a0acaafe61
Laura <laura.kaliyeva@nu.edu.kz>
2020-05-25 13:24:34 +0600

    add pages, navigation and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Navbar} from './components/Navbar'
+import {Home} from './pages/Home'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {About} from './pages/About'
+import {Profile} from './pages/Profile'
 
 function App() {
   return (
-    <div>
-    </div>
+      <BrowserRouter>
+          <Navbar />
+          <div className="container pt-4">
+              <Switch>
+                  <Route path="/" exact component={Home} />
+                  <Route path="/about" component={About} />
+                  <Route path="/profile/:name" component={Profile} />
+              </Switch>
+          </div>
+      </BrowserRouter>
   );
 }
 
