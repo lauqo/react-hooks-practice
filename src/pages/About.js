ed01bcfa36d2cbcbb946844b5c87e52f56952169
Laura <laura.kaliyeva@nu.edu.kz>
2020-05-27 22:16:30 +0600

    complete about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,8 +3,12 @@ import React from 'react'
 export const About = () => {
 
     return (
-        <div>
-            About page
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Github search app</h1>
+                <p className="lead">This is a simple React app that uses github API to demonstrate the
+                beauty and power of using <strong>hooks</strong>.</p>
+            </div>
         </div>
     )
 }
