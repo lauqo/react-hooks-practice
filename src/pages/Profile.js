fd0ff710f43e6dadce66ee00e72344b71c5d21a0
Laura <laura.kaliyeva@nu.edu.kz>
2020-05-27 22:11:41 +0600

    complete profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,66 @@
-import React from 'react'
+import React, {Fragment, useContext, useEffect} from 'react'
+import {GithubContext} from '../context/github/githubContext'
+import {Link} from 'react-router-dom'
+import {Loader} from '../components/Loader'
+import {Repos} from '../components/Repos'
 
-export const Profile = () => {
+export const Profile = ({match}) => {
+
+    const {getUser, getRepos, user, loading, repos} = useContext(GithubContext)
+
+    const userName = match.params.name
+
+    useEffect(() => {
+        getUser(userName)
+        getRepos(userName)
+        // eslint-disable-next-line
+    }, [])
+
+    if (loading) {
+        return <Loader/>
+    }
+
+    const {
+        name, company, bio, blog, location, login, public_repos,
+        avatar_url, html_url, followers, following, public_gists
+    } = user
 
     return (
-        <div>
-            Profile page
-        </div>
+        <Fragment>
+            <Link to="/" className="btn btn-link">Go to Main</Link>
+            <div className="card mb-4">
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-sm-3 text-center">
+                            <img src={avatar_url} alt={login} style={{width: '150px'}}/>
+                            <h1>{name}</h1>
+                            {location && <p>Location: {location}</p>}
+                        </div>
+                        <div className="col">
+                            {
+                                bio && <Fragment>
+                                    <h3>Bio</h3>
+                                    <p>{bio}</p>
+                                </Fragment>
+                            }
+                            <a href={html_url}
+                               target="_blank"
+                               rel="noopener noreferrer"
+                               className="btn btn-primary">Open github profile</a>
+                            <ul>
+                                {login && <li><strong>Username: </strong>{login}</li>}
+                                {company && <li><strong>Company: </strong>{company}</li>}
+                                {blog && <li><strong>Website: </strong>{blog}</li>}
+                            </ul>
+                            <div className="badge badge-primary">Following: {following}</div>
+                            <div className="badge badge-dark">Followers: {followers}</div>
+                            <div className="badge badge-success">Repos: {public_repos}</div>
+                            <div className="badge badge-info">Gists: {public_gists}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Repos repos={repos}/>
+        </Fragment>
     )
 }
